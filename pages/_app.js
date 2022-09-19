641dbc748e2c3a1e7d00551ff9c1c5aba3d850bd
sugyinbrs <sgnglee@caretbio.com>
2022-09-19 17:53:44 +0900

    Next.js 에서 자주 쓰이는 패턴

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,18 @@ _app.js (프레임워크 내에 설정된 파일명)
 Next.js 가 모든 페이지를 렌더링 할 수 있게 하는 파일
 Next.js 확인 순서
 * _app.js -> index.js
+
+주로 google analytics, SEO 와 관련된 것 등등과 관련된 코드를 구성하는 곳이기에
+Layout 이라는 컴포넌트를 사용하여 가볍게 처리
  */
 
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Head from "next/head"; // Next.js 제공, React-Helmet 과 같은 역할
+import Seo from "../components/Seo";
+
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1>Hello</h1>
     </div>
   );
